1f4c75127d96e1c519079b6b2c479c462a32da59
Nguyen Tuan Son <tieuthantai3112@gmail.com>
2020-07-06 15:34:47 +0700

    First Commit of Project

---
+++ app/Models/Tour.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tour extends Model
+{
+    protected $fillable = [
+    	'name',
+    	'destination_id',
+    	'summary',
+    	'content',
+    	'image',
+    	'adult_price',
+    	'children_price',
+    	'duration'
+    ];
+    public $timestamps = false;
+
+    public function packages()
+    {
+        return $this->belongsToMany(Package::class,'package_details', 'tour_id', 'package_id');
+    }
+}
